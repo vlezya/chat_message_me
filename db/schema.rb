9b82836ffe1487ab14962e77d99fd6adf288ffb9
vlezya <vlezyashop@gmail.com>
2023-03-30 23:21:57 +0300

    Build Message resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_30_134931) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_30_150511) do
+  create_table "messages", force: :cascade do |t|
+    t.string "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
@@ -18,4 +26,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_30_134931) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "users"
 end
